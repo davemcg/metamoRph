959548ed38e1b4a46f07ef29deb30ddac89decb2
David McGaughey <mcgaughey@gmail.com>
2023-07-08 18:14:22 -0400

    importFrom added, swapped from 0 to 1 as best score,  removed nnet model, fixed bug in num_PCs

--- R/modeling.R
+++ R/modeling.R
@@ -14,9 +14,13 @@
 #' @param num_PCs number of principal components to use from the training_data.
 #' Defaults to the first (top) 200.
 #' @param BPPARAM The BiocParallel class
-#' @param model lm or glm. We strongly recommend using lm.
+#' @param model Default is lm. We also support xgboost, glm, rf, and svm.
+#' In our experience we find lm and svm to be the best performers.
 #' @param verbose Print training status for each label type
 #' @return A list of models for each individual label type
+#' @import BiocParallel
+#' @import tidymodels
+#' @import parsnip
 #' @export
 model_build <- function(training_data,
                         training_labels,
@@ -29,11 +33,11 @@ model_build <- function(training_data,
   if (ncol(training_data) < num_PCs){
     warning(paste0("Dropping num PCs requested from ",
                    num_PCs, " to number of columns in training data"))
+    num_PCs <- ncol(training_data)
   }
-  num_PCs <- min(ncol(training_data), num_PCs)
   training_data <- training_data[,1:num_PCs]
   require(BiocParallel)
-  models <- lapply(unique(sort(training_labels)), function(target) {
+  models <- bplapply(unique(sort(training_labels)), function(target) {
     if (verbose){
       print(paste0("Model training for ", target))
     }
@@ -41,29 +45,35 @@ model_build <- function(training_data,
     binary_training_labels[binary_training_labels != target] <- 'Other'
 
     labels <- binary_training_labels %>%
-      factor(levels = c(target, "Other")) %>%
+      factor(levels = c("Other", target)) %>%
       as.integer() - 1
-    # if (model == 'xgboost'){
-    # nrounds = 200
-    # early_stopping_rounds = 10
-    # subsample = 0.5
-    # xg_verbose = FALSE
-    # eta = 0.3
-    #   model <- xgboost(data = training_data,
-    #                    label = labels,
-    #                    nrounds = nrounds,
-    #                    early_stopping_rounds = early_stopping_rounds,
-    #                    subsample = subsample,
-    #                    eta = eta,
-    #                    objective = 'binary:logistic',
-    #                    verbose = xg_verbose)
-    # }
+
     if (model == 'lm'){
       model <- lm(labels ~ ., data.frame(cbind(labels,training_data)))
     } else if (model == 'glm'){
-      mdata <- data.frame(cbind(labels,training_data))
-      mdata$labels <- as.factor(mdata$labels)
-      model <- glm(labels ~ ., data = mdata, family = binomial(link='log'))
+      model <- glm(labels ~ ., data = data.frame(cbind(labels, training_data)),family = binomial("logit"))
+    } else if (model == 'xgboost'){
+      model <- boost_tree(mode = "regression", trees = 200) %>%
+        fit(labels ~ .,
+            data = data.frame(cbind(labels,training_data)))
+    # } else if (model == 'nnet'){
+    #   model <- mlp() %>%
+    #     set_engine("nnet") %>%
+    #     set_mode("regression") %>%
+    #     fit(labels ~ .,
+    #         data = data.frame(cbind(labels,training_data)))
+    } else if (model == 'rf'){
+      model <-  rand_forest(trees = 500, min_n = 5) %>%
+        set_mode("regression") %>%
+        set_engine("ranger") %>%
+        fit(labels ~ .,
+            data = data.frame(cbind(labels,training_data)))
+    } else if (model == 'svm'){
+      model <- svm_linear(cost = 1, margin = 0.1) %>%
+        set_mode("regression") %>%
+        set_engine("LiblineaR") %>%
+        fit(labels ~ .,
+            data = data.frame(cbind(labels,training_data)))
     }
     return(list(target = target, model = model))
   })
@@ -71,7 +81,7 @@ model_build <- function(training_data,
   for (i in 1:length(models)){
     model_out[[models[[i]]$target]] <- models[[i]]$model
   }
-  model_out
+  return(model_out)
 }
 
 
@@ -79,13 +89,17 @@ model_build <- function(training_data,
 #'
 #' This function uses the output from [eigenProjectR::model_build()]
 #'
-#' @param list_of_models sample (row) by principal component (column) matrix
+#' @param list_of_models list object containing one model per sample type (e.g. photoreceptors vs not-photoreceptors)
 #' @param experiment_data Projected data [eigenProjectR::eigenProjectR()]
 #' @param experiment_labels Optional labels for the users experiment_data
 #' @param return_predictions By default the predicted labels are returned.
-#' If set to TRUE, then the entire matrix of probabilites for each sample (against
+#' If set to TRUE, then the entire matrix of probabilities for each sample (against
 #' each label type) is returned.
-#' @return By default, a table of predicted labels for your data
+#' @return By default, a table of predicted labels for your data, if return_predictions set to TRUE then
+#' a matrix of probabilities for each sample type is returned (where rows are samples and columns
+#' are each sample type probability)
+#' @importFrom stats predict
+#' @importFrom dplyr bind_cols as_tibble mutate relocate case_when
 #' @export
 model_apply <- function(list_of_models,
                         experiment_data,
@@ -99,24 +113,24 @@ model_apply <- function(list_of_models,
   # turn predictions into a tibble
   predictions <- predictions %>% bind_cols()
   colnames(predictions) <- names(list_of_models)
-  # identify the most likely celltype call (lowest value) for each sample
+  # identify the most likely celltype call (highest value) for each sample
   calls <- cbind(row.names(experiment_data),
-                 colnames(predictions)[apply(predictions, 1, which.min)],
-                 apply(predictions, 1, min)) %>%
+                 colnames(predictions)[apply(predictions, 1, which.max)],
+                 apply(predictions, 1, max)) %>%
     data.frame()
-  colnames(calls) <- c('sample_id', 'predict', 'min_score')
+  colnames(calls) <- c('sample_id', 'predict', 'max_score')
   # optionally put in true labels for input data (if given)
   if (experiment_labels[1] != ''){
     calls$sample_label <- experiment_labels
-    colnames(calls) <- c('sample_id', 'predict', 'min_score', 'sample_label')
+    colnames(calls) <- c('sample_id', 'predict', 'max_score', 'sample_label')
     calls <- calls %>% relocate(sample_id, sample_label)
   }
 
   calls <- as_tibble(calls) %>%
-    mutate(min_score = as.numeric(min_score),
-           predict_stringent = case_when(min_score > 0.5 ~ 'Unknown',
+    mutate(max_score = as.numeric(max_score),
+           predict_stringent = case_when(max_score < 0.5 ~ 'Unknown',
                                          TRUE ~ predict)) %>%
-    relocate(min_score, .after = predict_stringent)
+    relocate(max_score, .after = predict_stringent)
   if (return_predictions){
     predictions
   } else{
